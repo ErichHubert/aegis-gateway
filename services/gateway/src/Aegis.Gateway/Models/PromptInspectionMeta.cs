160747c049e8c6386b462d25f4e1ad381f0b1b20
Erich Hubert <erich.hubert@gmx.de>
2025-11-22 19:28:09 +0100

    Refactored folder structure

--- services/gateway/src/Aegis.Gateway/Models/PromptInspectionMeta.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Aegis.Gateway.Models;
-
-internal class PromptInspectionMeta
-{
-    [JsonPropertyName("userId")]
-    public string? UserId { get; set; }
-}
