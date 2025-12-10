ff8948307fb8066d5a6cb1ac62ba6391a99a164c
Erich Hubert <erich.hubert@gmx.de>
2025-12-10 17:00:40 +0100

    fixed telegram issues

--- services/inspection/tests/secret/secret_telegram_tests.py
+++ services/inspection/tests/secret/secret_telegram_tests.py
@@ -13,12 +13,11 @@ def _get_finding(findings, type_id: str):
 @pytest.mark.parametrize(
     'secret, should_flag',
     [
-        ('bot110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', False),
+        ('bot110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', True),
         ('110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', True),
         ('7213808860:AAH1bjqpKKW3maRSPAxzIU-0v6xNuq2-NjM', True),
         ('foo:AAH1bjqpKKW3maRSPAxzIU-0v6xNuq2-NjM', False),
         ('foo', False),
-        ('arn:aws:sns:aaa:111122223333:aaaaaaaaaaaaaaaaaaassssssddddddddddddd', False),
     ],
 )
 def test_telegrambottoken_detector_detects_expected_secrets(secret: str, should_flag: bool):
