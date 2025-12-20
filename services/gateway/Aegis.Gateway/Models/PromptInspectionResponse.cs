8ba64b1c1f0e25679d968af66d7ff31f1352df2e
Erich Hubert <erich.hubert@gmx.de>
2025-12-20 20:12:25 +0100

    Removed isAllowed due to policies in gateway

--- services/gateway/Aegis.Gateway/Models/PromptInspectionResponse.cs
+++ services/gateway/Aegis.Gateway/Models/PromptInspectionResponse.cs
@@ -4,9 +4,6 @@ namespace Aegis.Gateway.Models;
 
 public sealed class PromptInspectionResponse
 {
-    [JsonPropertyName("isAllowed")]
-    public bool IsAllowed { get; set; } = false; 
-
     [JsonPropertyName("findings")]
     public List<PromptInspectionFinding> Findings { get; set; } = [];
 }
